30cd938c4c7bed14eac7bb8a9830898221c5d8bd
theeKirat1984 <amazingSevenfix@gmail.com>
2025-09-19 03:14:33 +0500

    feat: header

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,7 +5,6 @@
         elevation="3"
         rounded="md"
         class="d-flex align-center px-4 py-2 mt-4"
-        style="background-color: rgba(255, 255, 255, 0.85);"
       >
         <span class="text-h6 font-weight-bold">World-Extreame.com</span>
         <v-spacer />
@@ -14,11 +13,22 @@
         <v-btn variant="text">Контакты</v-btn>
       </v-sheet>
     </v-container>
-    <v-container class="text-center mt-12">
-      <h1 class="text-h2 font-weight-bold  mt-4" style="color:rgba(000, 000, 000, 0.5)">Добро пожаловать</h1>
-      <p class="text-h6 text-black">
-        все новые экстримальные новости здесь
-      </p>
+    <v-container class="text-center">
+      <v-row 
+        no-gutters
+        style="height: 300px;"
+      >
+        <v-col align-self="end"  style="background-color: rgba(999, 999, 999, 0.7);">
+          <v-sheet 
+            color="transparent"
+          >
+            <h1 class="text-h2 font-weight-bold mt-4" style="color:rgba(000, 000, 000, 0.8)">Добро пожаловать</h1>
+            <p class="text-h6 text-black">
+              все новые экстримальные новости здесь
+            </p>
+          </v-sheet>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -27,7 +37,7 @@
 .header {
   height: 800px;
   background-image: url('/main/mountain.jpg');
-  /* background-position: 0 -500px; */
+  background-position: 0 -70px;
   background-size: cover;
 }
 </style>
