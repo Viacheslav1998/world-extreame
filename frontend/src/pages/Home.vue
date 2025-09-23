0ee431905723c650e0d4b34d166157ae609310a8
theeKirat1984 <amazingSevenfix@gmail.com>
2025-09-23 19:58:40 +0500

    feat: preparing

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,12 +1,12 @@
 <template>
-	<!-- pheraps need another windows -->
+	<!-- pheraps need slots? -->
 	<v-container>
-		<v-row no-gutters class="bg-deep-purple-lighten-4">
+		<v-row no-gutters class="bg-blue-grey-lighten-3">
       <v-col cols="7">
         <v-row>
           <v-col>
             <v-sheet class="pa-2 ma-2 rounded">
-              <h2>Быстрый магазин</h2>
+              <h2 class="text-red-darken-4">Быстрый магазин</h2>
             </v-sheet>
           </v-col>
         </v-row>
@@ -27,11 +27,21 @@
             </v-sheet>
           </v-col>
         </v-row>
+        
+        <v-row>
+          <div class="spacer pa-1 ma-1"></div>
+        </v-row>
+        <v-row no-gutters>
+          <v-col>
+            <v-sheet class="pa-2 ma-2 rounded">
+              <h3>Последние новости:</h3>
+            </v-sheet>
+          </v-col>
+        </v-row>
       </v-col>
 
-
       <v-col>
-        <v-sheet class="pa-2 ma-2">
+        <v-sheet class="pa-2 ma-2 rounded">
           .v-col-auto
           тут какие то быстрые решения
           рекламки баннеры
@@ -44,3 +54,7 @@
 <script setup lang="ts">
 	
 </script>
+
+<style scoped>
+
+</style>
