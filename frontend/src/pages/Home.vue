7c24a7a7819d6e96d88ba947a9fa61a9a22ae9f4
theeKirat1984 <amazingSevenfix@gmail.com>
2025-09-23 20:21:12 +0500

    feat: css, boxes, grid, fix

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,39 +1,40 @@
 <template>
 	<!-- pheraps need slots? -->
 	<v-container>
-		<v-row no-gutters class="bg-blue-grey-lighten-3">
+		<v-row no-gutters class="bg-blue-grey-lighten-5">
       <v-col cols="7">
         <v-row>
           <v-col>
             <v-sheet class="pa-2 ma-2 rounded">
-              <h2 class="text-red-darken-4">Быстрый магазин</h2>
+              <h2 class="text-blue-grey">Быстрый магазин</h2>
             </v-sheet>
           </v-col>
         </v-row>
         <v-row no-gutters>
           <v-col>
-            <v-sheet class="pa-2 ma-2 rounded">
+            <v-sheet class="pa-2 ma-2 rounded m-forest">
+              <h4>магазин</h4>
+              <p>тут хороший стаф</p>
+            </v-sheet>
+          </v-col>
+          <v-col>
+            <v-sheet class="pa-2 ma-2 rounded m-bycucle">
               asd
             </v-sheet>
           </v-col>
           <v-col>
-            <v-sheet class="pa-2 ma-2 rounded">
-              asd
-            </v-sheet>
-          </v-col>
-          <v-col>
-            <v-sheet class="pa-2 ma-2 rounded">
+            <v-sheet class="pa-2 ma-2 rounded m-cross">
               asd
             </v-sheet>
           </v-col>
         </v-row>
-        
+
         <v-row>
           <div class="spacer pa-1 ma-1"></div>
         </v-row>
         <v-row no-gutters>
           <v-col>
-            <v-sheet class="pa-2 ma-2 rounded">
+            <v-sheet class="pa-2 ma-2 rounded text-blue-grey">
               <h3>Последние новости:</h3>
             </v-sheet>
           </v-col>
@@ -56,5 +57,18 @@
 </script>
 
 <style scoped>
-
+.m-bycucle, .m-forest, .m-cross {
+  height: 100px;
+  background-size: cover;
+  background-position: center;
+}
+.m-forest {
+  background-image: url('/main/f.jpg');
+}
+.m-bycucle {
+  background-image: url('/main/bc.jpg');
+}
+.m-cross {
+  background-image: url('/main/c.jpg');
+}
 </style>
