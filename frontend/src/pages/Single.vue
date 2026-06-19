2b10295f5eb37864112b5de7d087ad71a4a69d6c
FM4GLOCK <amazingSevenfix@gmail.com>
2026-06-19 22:52:13 +0500

    feat: breadcrumbs

--- frontend/src/pages/Single.vue
+++ frontend/src/pages/Single.vue
@@ -1,14 +1,45 @@
 <template>
   <v-container>
-    
+    <div>
+      <v-breadcrumbs 
+        bg-color="indigo-lighten-1"
+        :items="items"
+        divider="/"
+      ></v-breadcrumbs>
+    </div>
   </v-container>
 </template>
 
 <script setup lang="ts">
   import { useRoute } from 'vue-router';
+
   const route = useRoute();
   const postId = route.params.id;
   console.log('Post ID:', postId);
+
+  interface Item {
+    title: string, 
+    disabled: boolean,
+    href: string
+  }
+
+  const items: Item[] = [
+    {
+      title: 'Главная',
+      disabled: false, 
+      href: '/'
+    },
+    {
+      title: 'новости',
+      disabled: false, 
+      href: '/news'
+    },
+    {
+      title: 'текущая новость',
+      disabled: true, 
+      href: '/current'
+    },
+  ]
 
 </script>
 
