f37059127af2f1c39ae7a815e4e68ce4998407b0
FM4GLOCK <amazingSevenfix@gmail.com>
2026-07-23 22:53:23 +0500

    feat: refactor scroll, fix, design ...

--- frontend/src/pages/layouts/Main.vue
+++ frontend/src/pages/layouts/Main.vue
@@ -1,50 +1,56 @@
 <template>
   <!-- static SFC | don`t dynamic theme! -->
-  <v-app>
-    <v-main class="d-flex align-center ">
-      <v-container class="md-6">
+ <v-app>
+    <v-main class="d-flex align-center">
+      <v-container>
         <div class="text-button text-grey-darken-1 font-weight-bold pb-2">
           Навигация по сайту и сервисы
         </div>
-        <v-row justify="space-around">
-          <v-col 
-            align-self="start"
-            v-for="(column, colIndex) in optionsCards"
-            :key="colIndex"
-          >
+        <v-row justify="center">
+          <v-col cols="12" md="9">
             <v-row>
-              <v-col class="d-flex flex-column ga-2">
-
-                <v-card
-                  v-for="card in column"
-                  :key="card.id"
-                  :disabled="card.disabled"
-                  :to="card.link"
-                  class="h-100 d-flex flex-column mb-5"
-                  elevation="2"
-                >
-                  <v-img
-                    :src="card.image"
-                    height="150"
-                    cover
-                    class="text-white"
-                  ></v-img>
-                  <v-card-item :class="['flex-grow-1', card.color]">
-                    <div class="text-overline">
-                      {{ card.title }}
-                    </div>
-                  </v-card-item>
-                </v-card>
+              <v-col 
+                v-for="(column, colIndex) in optionsCards"
+                :key="colIndex"
+                cols="12"
+                sm="4"
+                md="4"
+              >
+                <div class="d-flex flex-column ga-4 h-100">
+                  <v-card
+                    v-for="card in column"
+                    :key="card.id"
+                    :disabled="card.disabled"
+                    :to="card.link"
+                    class="card-item d-flex flex-column"
+                    elevation="2"
+                  >
+                    <v-img
+                      :src="card.image"
+                      height="150"
+                      cover
+                      class="text-white"
+                    ></v-img>
+                    <v-card-item :class="['flex-grow-1', card.color]">
+                      <div class="text-overline">
+                        {{ card.title }}
+                      </div>
+                    </v-card-item>
+                  </v-card>
+                </div>
               </v-col>
             </v-row>
           </v-col>
-          <v-date-picker
-            class="ma-2 text-grey-darken-1"
-            v-model="date"
-            :allowed-dates="allowedDates"
-            :min="minDate"
-            :max="maxDate"
-          ></v-date-picker>
+          <v-col cols="12" md="3" class="d-flex justify-center justify-md-start">
+            <v-date-picker
+              class="ma-2 text-grey-darken-1 w-100"
+              style="max-width: 320px;"
+              v-model="date"
+              :allowed-dates="allowedDates"
+              :min="minDate"
+              :max="maxDate"
+            ></v-date-picker>
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
@@ -52,10 +58,13 @@
 </template>
 
 <style scoped> 
+ .card-item {
+    flex: 1 1 0%;
+  }
 </style>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue';
+import { ref } from 'vue';
 
 interface OptionsCard {
   id: number;
@@ -72,7 +81,7 @@ const optionsCards = ref<OptionsCard[][]>([
       id: 1,
       link: '/',
       title: 'Главные новости спорта',  
-      image: '../../public/navi/m6.jpg',
+      image: '/navi/m6.jpg',
       color: 'bg-purple',
       disabled: false
     },
@@ -80,7 +89,7 @@ const optionsCards = ref<OptionsCard[][]>([
       id: 2,
       link: '/news1',
       title: 'Магазин вело-мото',  
-      image: '../../public/navi/m3.jpg',
+      image: '/navi/m3.jpg',
       color: 'bg-blue-grey-darken-4',
       disabled: false
     },
@@ -91,7 +100,7 @@ const optionsCards = ref<OptionsCard[][]>([
       id: 3,
       link: '/news1',
       title: 'Спортивный инвентарь',  
-      image: '../../public/navi/m4.jpg',
+      image: '/navi/m4.jpg',
       color: 'bg-indigo-darken-2',
       disabled: true
     },
@@ -99,7 +108,7 @@ const optionsCards = ref<OptionsCard[][]>([
       id: 4,
       link: '/news1',
       title: 'Партнеры',  
-      image: '../../public/navi/m5.jpg',
+      image: '/navi/m5.jpg',
       color: 'bg-teal',
       disabled: true
     },
@@ -110,7 +119,7 @@ const optionsCards = ref<OptionsCard[][]>([
       id: 5,
       link: '/news1',
       title: 'Мерч',  
-      image: '../../public/navi/m1.jpg',
+      image: '/navi/m1.jpg',
       color: 'bg-pink',
       disabled: true
     },
@@ -118,7 +127,7 @@ const optionsCards = ref<OptionsCard[][]>([
       id: 6,
       link: '/news1',
       title: 'Сообщества',  
-      image: '../../public/navi/m2.jpg',
+      image: '/navi/m2.jpg',
       color: 'bg-deep-orange',
       disabled: true
     },
@@ -137,15 +146,4 @@ const allowedDates = (val: unknown): boolean => {
   }
   return false;
 };
-
-onUnmounted(() => {
-  document.documentElement.style.overflow = '' // Safari
-  document.body.style.overflow = '';
-})
-
-onMounted(() => {
-  document.documentElement.style.overflow = 'hidden'
-  document.body.style.overflow = 'hidden';
-})
-
 </script>
