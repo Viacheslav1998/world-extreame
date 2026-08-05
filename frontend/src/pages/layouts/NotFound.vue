f4af82b7794710ed73e6bdc51c6e4026a95acd9d
FM4GLOCK <amazingSevenfix@gmail.com>
2026-08-05 14:23:52 +0500

    feat: 404 page

--- frontend/src/pages/layouts/NotFound.vue
+++ frontend/src/pages/layouts/NotFound.vue
@@ -1,15 +1,33 @@
 <template>
- <v-app>
-    <v-main class="d-flex align-center">
+ <v-app >
+    <v-main
+      style="background-image: url('/notfound/404.jpg');"
+      class="d-flex align-center error-page">
       <v-container>
-        <h1>NotFound</h1>
-        <i>404</i>
+        <div class="text-white mb-5">
+          <h1>Такой страницы нет</h1>
+          <h2>404</h2>
+        </div>
+        <div>
+         <v-btn 
+           variant="outlined"
+           class="text-white"
+           to="/">
+             выход от сюда
+          </v-btn>
+        </div>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <style scoped> 
+.error-page {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+
+}
 </style>
 
 <script setup lang="ts">
